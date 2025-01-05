a79182394a24e4faeca99336d0c6bf2412673c6a
Beeamo <56599383+Beeamo@users.noreply.github.com>
2025-01-05 18:42:19 -0500

    Add Rounds logic

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-
-let humanScore = 0
-let computerScore = 0
-
-
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1
     switch (choice) {
@@ -30,3 +25,64 @@ function getHumanChoice() {
     }
 }
 
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    let rounds = 5
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log("You both picked the same, You tie this round");
+            return;
+        }
+        if (humanChoice > computerChoice) {
+            if (humanChoice == 2 && computerChoice == 0) {
+                console.log("Computer wins! Rock beats scissor.");
+                computerScore++;
+                return;
+            }
+            switch (humanChoice) {
+                case 1:
+                    console.log("You win! Paper beats Rock");
+                    humanScore++;
+                    return;
+                case 2:
+                    console.log("You win! Scissor beats Paper");
+                    humanScore++;
+                    return;
+            }
+        }
+    
+        if (computerChoice == 2 && humanChoice == 0) {
+            console.log("You wins! Rock beats Scissor.");
+            humanScore++;
+            return;
+        }
+        switch (computerChoice) {
+            case 1:
+                console.log("Computer wins! Paper beats Rock");
+                computerScore++;
+                return;
+            case 2:
+                console.log("Computer wins! Scissor beats Paper");
+                computerScore++;
+                return;
+    
+        } 
+    }
+
+    for (let i = 0; i < rounds; i) {
+        const currentHumanScore = humanScore
+        const currentComputerScore = computerScore
+        playRound(getHumanChoice(), getComputerChoice())
+        if (currentComputerScore != computerScore || currentHumanScore != humanScore) {
+            i++;
+        }
+    }
+
+
+
+}
+
+playGame()
